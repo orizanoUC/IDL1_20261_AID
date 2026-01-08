0f67f0031f420fd7dafc07fd7168618b013d75ac
Sergio Victor Orizano Salvador <sorizano@continental.edu.pe>
2026-01-07 22:07:01 -0500

    agregando gráfico

--- app_graficos.py
+++ app_graficos.py
@@ -51,3 +51,42 @@ else:
     st.error("El archivo CSV debe contener las columnas: " + str(columnas_necesarias))
 
     
+st.title("Correlación de Pearson - ventas")
+
+if archivo is not None:
+    df = pd.read_csv(archivo)
+    
+    st.subheader("Variables numéricas disponibles")
+    vars_numericas = df.select_dtypes(include='number').columns.tolist()
+    st.write(vars_numericas)
+
+    var_x = st.selectbox("Selecciona la primera variable", vars_numericas)
+    var_y = st.selectbox("Selecciona la segunda variable", vars_numericas)
+
+    if var_x != var_y:
+        correlacion = df[var_x].corr(df[var_y], method='pearson')
+
+        st.metric(
+            label="Correlación entre {var_x} y {var_y}",
+            value=round(correlacion, 3)
+        )
+
+        #Interpretación automática
+        if abs(correlacion) >= 0.7:
+            st.warning("Correlación fuerte")
+        elif abs(correlacion) >= 0.4:
+            st.info("Correlación moderada")
+        elif abs(correlacion) >= 0.2:
+            st.success("Correlación débil")
+        else:
+            st.write("Correlación nula o inexistente")
+
+        st.info("Interpretación: {interpretacion}")
+
+    else:
+        st.error("Seleccione dos variables diferentes")
+else:
+    st.error("Sube un archivo CSV")
+
+
+
